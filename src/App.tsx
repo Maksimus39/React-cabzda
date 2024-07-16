5f67d287ba13157673ea7d237067b28fc79bf809
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-07-16 18:49:45 +0300

    01 - Компоненты (часть 2)

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,28 @@ import React from 'react';
 import './App.css';
 
 function App() {
+    console.log("App rendering")
     return (
         <div className="App">
+            <AppTitle/>
             <Rating/>
             <Accordion/>
+            <Rating/>
         </div>
     );
 }
 
+function AppTitle() {
+    console.log("AppTitle rendering")
+    return (
+        <>
+            This is App component
+        </>
+    )
+}
+
 function Rating() {
+    console.log("Rating rendering")
     return (
         <div>
             <Star/>
@@ -23,15 +36,29 @@ function Rating() {
 }
 
 function Star() {
-    return (
-        <div>Star</div>
-    )
+    console.log("Star rendering")
+    return <div>Star</div>
 }
 
 function Accordion() {
+    console.log("Accordion rendering")
     return (
         <div>
-            <h3>Menu</h3>
+            <AccordionTitle/>
+            <AccordionBody/>
+        </div>
+    )
+}
+
+function AccordionTitle() {
+    console.log("AccordionTitle rendering")
+    return <h3>Menu</h3>
+}
+
+function AccordionBody() {
+    console.log("AccordionBody rendering")
+    return (
+        <div>
             <ul>
                 <li>1</li>
                 <li>2</li>
