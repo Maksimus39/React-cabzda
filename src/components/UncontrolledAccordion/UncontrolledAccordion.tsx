3026433b77c622e4aac57997ff0f8ad8c786c095
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-07-20 21:45:51 +0300

    Рефактор компонент UncontrolledAccordion

--- src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
+++ src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
@@ -2,34 +2,36 @@ import React from "react";
 
 type AccordionProps = {
     titleValue: string
-
 }
 
 export function UncontrolledAccordion(props: AccordionProps) {
     //debugger
     console.log("Accordion rendering")
-
-    const [collapsed, setCollapsed] = React.useState(true)
-
-    function toggleHeader() {
-        setCollapsed(!collapsed)
-    }
+    const [collapsed, setCollapsed] = React.useState(false)
 
     return (
         <div>
-            <AccordionTitle title={props.titleValue}/>
-            <button onClick={toggleHeader}>TOGGLE</button>
+            <AccordionTitle title={props.titleValue} setCollapsed={setCollapsed} collapsed={collapsed}/>
             {collapsed && <AccordionBody/>}
         </div>
     )
 }
 
-type AccordionTitleProps = { title: string }
+type AccordionTitleProps = {
+    title: string
+    setCollapsed: (value: boolean) => void
+    collapsed: boolean
+}
 
 function AccordionTitle(props: AccordionTitleProps) {
     //debugger
-    console.log("AccordionTitle rendering")
-    return <h3>{props.title}</h3>
+    console.log("AccordionTitle rendering");
+
+    function accordionCollapsedHandler() {
+        props.setCollapsed(!props.collapsed)
+    }
+
+    return <h3 onClick={accordionCollapsedHandler}>{props.title}</h3>
 }
 
 
