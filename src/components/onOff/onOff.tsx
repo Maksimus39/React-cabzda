9b18c91c8760cc8b735155eb8a6960364c5fb7a7
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-07-20 14:16:18 +0300

    Написали логику для OnOff

--- src/components/onOff/onOff.tsx
+++ src/components/onOff/onOff.tsx
@@ -1,9 +1,53 @@
 import React from 'react';
 
-export const OnOff = () => {
+
+export function OnOff(): React.ReactElement {
+
+    console.log('onOff rendering')
+
+    const [on, setOn] = React.useState(true);
+    console.log('on: ' + on)
+
+    function onClickHeader() {
+        setOn(true)
+    }
+
+    function offClickHeader() {
+        setOn(false)
+    }
+
+    const onStyle = {
+        width: "30px",
+        height: "30px",
+        border: "1px solid black",
+        display: "inline",
+        padding: "2px",
+        backgroundColor: on ? "green" : "white",
+    };
+    const offStyle = {
+        width: "30px",
+        height: "30px",
+        border: "1px solid black",
+        display: "inline",
+        marginLeft: "2px",
+        padding: "2px",
+        backgroundColor: on ? "white" : "red",
+    };
+    const indicatorStyle = {
+        width: "10",
+        height: "10",
+        borderRadius: "5px",
+        border: "1px solid black",
+        display: "inline",
+        marginLeft: "5px",
+        backgroundColor: on ? "green" : "red",
+    };
+
     return (
         <div>
-
+            <div style={onStyle} onClick={onClickHeader}>ON</div>
+            <div style={offStyle} onClick={offClickHeader}>OFF</div>
+            <div style={indicatorStyle}>CIRCLE</div>
         </div>
     );
 };
