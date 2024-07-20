f65f70b4f2ea44966076a5dbc3af956c7e070262
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-07-20 21:45:15 +0300

    Рефактор компонент OnOff

--- src/components/rating/Rating.tsx
+++ src/components/rating/Rating.tsx
@@ -1,30 +1,33 @@
 import React from "react";
 
-type ValueTypeRating = 0 | 1 | 2 | 3 | 4 | 5
-type RatingProps = { value: ValueTypeRating };
+export type ValueTypeRating = 0 | 1 | 2 | 3 | 4 | 5
+type RatingProps = {
+    value: ValueTypeRating
+    onClick: (value: ValueTypeRating) => void
+};
 
 export function Rating(props: RatingProps) {
     console.log("Rating rendering")
     return (
         <div>
-            <Star selected={props.value > 0}/>
-            <Star selected={props.value > 1}/>
-            <Star selected={props.value > 2}/>
-            <Star selected={props.value > 3}/>
-            <Star selected={props.value > 4}/>
+            <Star selected={props.value > 0} onClick={props.onClick} value={1}/>
+            <Star selected={props.value > 1} onClick={props.onClick} value={2}/>
+            <Star selected={props.value > 2} onClick={props.onClick} value={3}/>
+            <Star selected={props.value > 3} onClick={props.onClick} value={4}/>
+            <Star selected={props.value > 4} onClick={props.onClick} value={5}/>
         </div>
     )
 }
 
 type StarProps = {
     selected: boolean
+    onClick: (value: ValueTypeRating) => void
+    value: ValueTypeRating
 }
 
 function Star(props: StarProps) {
     console.log("Star rendering")
-    if (props.selected) {
-        return <span><b>Star </b></span>
-    } else {
-        return <span>Star </span>
-    }
+    return <span onClick={() => props.onClick(props.value)}>
+      {props.selected ? <b>star </b> : " star"}
+  </span>
 }
