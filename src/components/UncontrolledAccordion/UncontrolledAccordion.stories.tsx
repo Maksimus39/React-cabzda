5cdaaedbaccf6950cc7aa4688e871ac63d131bf7
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-09-04 09:31:41 +0300

    сторибук история на компонент UncontrolledAccordion

---
+++ src/components/UncontrolledAccordion/UncontrolledAccordion.stories.tsx
@@ -0,0 +1,11 @@
+import {UncontrolledAccordion} from "./UncontrolledAccordion";
+
+export default {
+    title: 'UncontrolledAccordion stories',
+    component: UncontrolledAccordion,
+}
+
+export const EmptyUncontrolledAccordion = () => {
+    return <UncontrolledAccordion titleValue={'UncontrolledAccordion'} />
+
+}
