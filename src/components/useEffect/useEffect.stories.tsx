0c528c27ff6a05896cedefd361f0b3151dbc2c57
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-10-08 14:43:01 +0300

    22 - useEffect \ setTimeout \ setInterval

--- src/components/useEffect/useEffect.stories.tsx
+++ src/components/useEffect/useEffect.stories.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 
 export default {
     title: 'useEffect demo'
@@ -6,28 +6,69 @@ export default {
 
 export const ExampleUseEffect = () => {
     const [counter, setCounter] = useState(1);
-    const [fake, setFAke] = useState(1);
+    const [fake, setFake] = useState(1);
 
-    console.log('ExampleUseEffect')
-
-    useEffect(() => {
-        console.log('useEffect every render')
-        document.title = counter.toString()
-    })
+    console.log('ExampleUseEffect');
 
     useEffect(() => {
-        console.log('useEffect only first render (componentDidMount)')
-        document.title = counter.toString()
-    }, [])
+        console.log('useEffect every render');
+        document.title = counter.toString();
+    });
 
     useEffect(() => {
-        console.log('useEffect  first render and every counter changed')
-        document.title = counter.toString()
-    }, [counter])
+        console.log('useEffect only first render (componentDidMount)');
+        document.title = counter.toString();
+    }, []);
 
-    return <>
-        Hello: {counter}
-        <button onClick={() => setFAke(fake + 1)}>Add face: {fake}</button>
-        <button onClick={() => setCounter(counter + 1)}>Add counter: {counter}</button>
-    </>
+    useEffect(() => {
+        console.log('useEffect first render and every counter changed');
+        document.title = counter.toString();
+    }, [counter]);
+
+    return (
+        <>
+            Hello: {counter}
+            <button onClick={() => setFake(fake + 1)}>Add fake: {fake}</button>
+            <button onClick={() => setCounter(counter + 1)}>Add counter: {counter}</button>
+        </>
+    );
+}
+
+export const SetTimeOutExample = () => {
+    const [counter, setCounter] = useState(1);
+    const [hours, setHours] = useState(new Date().getHours());
+    const [minutes, setMinutes] = useState(new Date().getMinutes());
+    const [seconds, setSeconds] = useState(new Date().getSeconds());
+
+    console.log('SetTimeOutExample');
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCounter((state) => state + 1);
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const currentDate = new Date();
+            setHours(currentDate.getHours());
+            setMinutes(currentDate.getMinutes());
+            setSeconds(currentDate.getSeconds());
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <>
+            <div>
+                Date: {counter}
+            </div>
+            <div>
+                Time: {hours}:{minutes}:{seconds}
+            </div>
+        </>
+    );
 }
